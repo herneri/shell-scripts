fd7dd3107fcc9e733e7a914d5911f2a4fbc26193
Eric Hernandez <109563290+herneri@users.noreply.github.com>
2022-07-19 12:29:23 -0500

    Added ddir.sh

---
+++ ddir.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ $1 == "-c" ]] && [[ $2 ]]; then
+    mkdir $2 && printf "$2\n"
+    
+    cd $2
+        mkdir $3 && printf "$3\n"
+    cd ..
+    exit 0
+elif [[ $1 == "-r" ]]; then
+    cd $2
+        rmdir $3 && printf "$3\n"
+    cd ..
+    
+    rmdir $2 && printf "$2\n"
+    exit 0
+elif [[ $1 == "-c" ]] || [[ $1 == "-r" ]]; then
+    printf "$0: Missing directory \n"
+    exit 1
+fi
+
+printf "Invalid option: $1 \n"
