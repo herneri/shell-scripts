5e30765872381fedb5b9aab4593e94308c2e5a23
Eric Hernandez <herneri@protonmail.com>
2022-07-21 12:46:13 -0500

    fix: Correct operation with a missing directory

--- ddir.sh
+++ ddir.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
-# Create two directories, one recursively
+# Create or remove two directories, one recursive
 set -e
 
-if [[ $1 == "-c" ]] && [[ $2 ]]; then
+c() {
     mkdir $2 && printf "$2\n"
-    
+
     cd $2
         mkdir $3 && printf "$3\n"
     cd ..
     exit 0
-elif [[ $1 == "-r" ]]; then
+}
+
+[[ $1 == "-c" ]] && [[ $2 ]] && [[ $3 ]] && c "$1" "$2" "$3"
+
+r() {
     cd $2
         rmdir $3 && printf "$3\n"
     cd ..
-    
+
     rmdir $2 && printf "$2\n"
     exit 0
-elif [[ $1 == "-c" ]] || [[ $1 == "-r" ]]; then
-    printf "$0: Missing directory \n"
-    exit 1
-fi
+}
 
-printf "Invalid option: $1 \n"
+[[ $1 == "-r" ]] && [[ $2 ]] && [[ $3 ]] && r "$1" "$2" "$3"
+
+[[ $1 == "-c" ]] || [[ $1 == "-r" ]] && printf "$0: Missing directory \n" && exit 1
+
+printf "Invalid option: '$1' \n"
