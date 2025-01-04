d16c1d9cb0787da64c063cb2122dece19324a43e
Eric Hernandez <herneri@protonmail.com>
2025-01-03 18:40:40 -0600

    feat: runw: Loop program execution

---
+++ runw.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# runw: Run a command when input is entered
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ ! $1 ]; then
+	printf "runw: runw [PROGRAM] \n"
+	exit 0
+fi
+
+cmd=$1
+while [ "$input" != "q" ]; do
+	read input
+	clear
+
+	if [ $input ] && [ $input != "q" ]; then
+		bash -c $cmd
+	fi
+done
